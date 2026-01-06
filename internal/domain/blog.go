2fdfaf602460f53071bf72efe95424006ac9f8bc
Hamiduzzaman96 <hamiduzzaman.ramim96@gmail.com>
2026-01-06 23:12:57 +0600

    blog created under domain

---
+++ internal/domain/blog.go
@@ -0,0 +1,28 @@
+package domain
+
+import "time"
+
+type BlogPost struct {
+	ID        uint
+	AuhtorID  uint
+	Title     string
+	Content   string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func NewBlogPost(authorId uint, title, content string) *BlogPost {
+	return &BlogPost{
+		AuhtorID:  authorId,
+		Title:     title,
+		Content:   content,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+}
+
+func (b *BlogPost) Update(title, content string) {
+	b.Title = title
+	b.Content = content
+	b.UpdatedAt = time.Now()
+}
