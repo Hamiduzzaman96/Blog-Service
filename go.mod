fb1d1f221090c25380674ec0fd04429921fa3a98
Hamiduzzaman96 <hamiduzzaman.ramim96@gmail.com>
2026-01-07 01:18:02 +0600

    models created for repository

--- go.mod
+++ go.mod
@@ -6,9 +6,12 @@ require (
 	github.com/joho/godotenv v1.5.1
 	google.golang.org/grpc v1.78.0
 	google.golang.org/protobuf v1.36.11
+	gorm.io/gorm v1.31.1
 )
 
 require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
---
+++ internal/repository/models.go
@@ -0,0 +1,32 @@
+package repository
+
+import "time"
+
+type UserModel struct {
+	ID       uint   `gorm:"primarykey;autoIncrement"`
+	Email    string `gorm:"unique; not null"`
+	Password string `gorm:"not null"`
+	Role     string `gorm:"not null"` // User // Author
+
+}
+
+type AuthorModel struct {
+	ID     uint `gorm:"primarykey;autoIncrement"`
+	UserID uint `gorm:"unique; not null"`
+}
+
+type BlogModel struct {
+	ID        uint   `gorm:"primarykey;autoIncrement"`
+	AuthorID  uint   `gorm:"not null"`
+	Title     string `gorm:"not null"`
+	Content   string `gorm:"type:text"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type NotificationModel struct {
+	ID      string `gorm:"primarykey; autoIncrement"`
+	UserID  uint   `gorm:"not null"`
+	Message string `gorm:"type:text"`
+	Sent    bool
+}
