834853791342a681f838e821cf614fc350d98d3b
Hamiduzzaman96 <hamiduzzaman.ramim96@gmail.com>
2026-01-09 01:12:59 +0600

    user handler created under http

---
+++ internal/handler/http/user_handler.go
@@ -0,0 +1,48 @@
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Hamiduzzaman96/Blog-Service/internal/usecase"
+)
+
+type UserHandler struct {
+	usecase *usecase.UserUsecase
+}
+
+func NewUserusecase(u *usecase.UserUsecase) *UserHandler {
+	return &UserHandler{
+		usecase: u,
+	}
+}
+
+func (h *UserHandler) Register(w http.ResponseWriter, r http.Request) {
+	var req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	json.NewDecoder(r.Body).Decode(&req)
+
+	user, err := h.usecase.Register(req.Email, req.Password)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	json.NewEncoder(w).Encode(user)
+}
+
+func (h *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	json.NewDecoder(r.Body).Decode(&req)
+
+	token, err := h.usecase.Login(req.Email, req.Password)
+	if err != nil {
+		http.Error(w, err.Error(), 401)
+		return
+	}
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
+}
